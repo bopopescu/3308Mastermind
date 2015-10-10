069d787c0d2c7ad1272718277075cd19b3fcdd9f
racheladdison <[rachel.a.lewis@colorado.edu]>
2015-10-10 17:16:36 -0600

    Adding peg holes and cover for solution

--- gameboard.py
+++ gameboard.py
@@ -1,12 +1,29 @@
 from graphics import *
 
+def board():
+	# background
+	win = GraphWin("My Rectangle", 800, 600)
+        r = Rectangle(Point(50, 25), Point(300, 550))
+        r.draw(win)
+        r.setFill(color_rgb(204, 102, 0))
+	cover = Rectangle (Point(70, 525), Point(210, 550))
+        cover.draw(win)
+        cover.setFill(color_rgb(0, 153, 76))
+	i = 0
+	while i < 12:
+		j = 0
+		while j < 4:
+			p = Circle(Point(80 + 40*j, 55 + 40*i), 10)
+			p.draw(win)
+			p.setFill('black')
+			j = j + 1
+		i = i + 1
+        win.getMouse() # pause for click in window
+        win.close()
+	
+
 def main():
-    win = GraphWin("My Rectangle", 800, 600)
-    r = Rectangle(Point(50, 50), Point(300, 550))
-    r.draw(win)
-    r.setFill('tan')
-    win.getMouse() # pause for click in window
-    win.close()
+	board()
 
 main()
 
