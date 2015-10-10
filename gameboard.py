3460f5f8e5a518368af39d2d91b57c257bc092e9
racheladdison <[rachel.a.lewis@colorado.edu]>
2015-10-10 17:30:16 -0600

    Adding smaller peg holes to show correctness of guess

--- gameboard.py
+++ gameboard.py
@@ -17,6 +17,18 @@ def board():
 			p.draw(win)
 			p.setFill('black')
 			j = j + 1
+		s1 = Circle(Point(225, 62 + 40*i), 5)
+		s1.draw(win)
+		s1.setFill('black')
+		s2 = Circle(Point(225, 48 + 40*i), 5)
+		s2.draw(win)
+		s2.setFill('black')
+		s3 = Circle(Point(239, 62 + 40*i), 5)
+		s3.draw(win)
+		s3.setFill('black')
+		s3 = Circle(Point(239, 48 + 40*i), 5)
+                s3.draw(win)
+                s3.setFill('black')
 		i = i + 1
         win.getMouse() # pause for click in window
         win.close()
