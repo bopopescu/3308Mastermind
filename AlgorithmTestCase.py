85e180e7ececa4bc087b09cfb04d0afd59843c08
Sonia Szeton <sonia.szeton@gmail.com>
2015-11-11 19:47:44 -0700

    Added Part 3 PDF

--- AlgorithmTestCase.py
+++ AlgorithmTestCase.py
@@ -7,9 +7,9 @@
 # Univerity of Colorado
 
 import unittest
-import mastermind_alg
-from mastermind_alg import Peg
-from mastermind_alg import Pin
+from mastermind_alg import *
+#from mastermind_alg import Peg
+#from mastermind_alg import Pin
 
 class AlgorithmTestCase(unittest.TestCase):
 
@@ -30,26 +30,50 @@ class AlgorithmTestCase(unittest.TestCase):
     def test_1(self):
         code = [Peg.blue, Peg.yellow, Peg.green, Peg.red]
         guess = [Peg.yellow, Peg.red, Peg.yellow, Peg.red]
-        score = mastermind_alg.scoreGuess(guess,code)
-        self.assertEqual(score, [Pin.white, Pin.black], "Incorrect score on test1: " + str(score))
+        score = scoreGuess(guess,code)
+        expected = [Pin.white, Pin.black]
+        print("TEST 1")
+        print("Code: " + str(code))
+        print("Guess: " + str(guess))
+        print("Actual Score: " + str(score))
+        print("Expected Score: " + str(expected))
+        self.assertEqual(score, expected, "Incorrect score on test1: " + str(score))
 
     def test_2(self):
         code = [Peg.yellow, Peg.yellow, Peg.blue, Peg.red]
         guess = [Peg.green, Peg.yellow, Peg.red, Peg.green]
-        score = mastermind_alg.scoreGuess(guess,code)
-        self.assertEqual(score, [Pin.black, Pin.white], "Incorrect score on test2: " + str(score))
+        score = scoreGuess(guess,code)
+        expected = [Pin.black, Pin.white]
+        print("TEST 2")
+        print("Code: " + str(code))
+        print("Guess: " + str(guess))
+        print("Actual Score: " + str(score))
+        print("Expected Score: " + str(expected))
+        self.assertEqual(score, expected, "Incorrect score on test2: " + str(score))
 
     def test_3(self):
         code = [Peg.orange, Peg.green, Peg.red, Peg.blue]
         guess = [Peg.blue, Peg.green, Peg.blue, Peg.orange]
-        score = mastermind_alg.scoreGuess(guess,code)
-        self.assertEqual(score, [Pin.white, Pin.black, Pin.white], "Incorrect score on test3: " + str(score))
+        score = scoreGuess(guess,code)
+        expected = [Pin.white, Pin.black, Pin.white]
+        print("TEST 3")
+        print("Code: " + str(code))
+        print("Guess: " + str(guess))
+        print("Actual Score: " + str(score))
+        print("Expected Score: " + str(expected))
+        self.assertEqual(score, expected, "Incorrect score on test3: " + str(score))
 
     def test_4(self):
         code = [Peg.orange, Peg.purple, Peg.red, Peg.purple]
         guess = [Peg.red, Peg.purple, Peg.empty, Peg.green]
-        score = mastermind_alg.scoreGuess(guess,code)
-        self.assertEqual(score, [Pin.black, Pin.white], "Incorrect score on test4: " + str(score))
+        score = scoreGuess(guess,code)
+        expected = [Pin.black, Pin.white]
+        print("TEST 4")
+        print("Code: " + str(code))
+        print("Guess: " + str(guess))
+        print("Actual Score: " + str(score))
+        print("Expected Score: " + str(expected))
+        self.assertEqual(score, expected, "Incorrect score on test4: " + str(score))
 
 
 # Main: Run Test Cases
